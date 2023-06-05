8739c17a4e42fcd883256731384c9361fabfdbd1
Janmajaya Mall <janmajayamall@protonmail.com>
2023-06-05 09:07:41 +0000

    add benches

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use hexl_rs::*;
+use rand::{
+    distributions::{Distribution, Uniform},
+    thread_rng,
+};
+
+fn random_values(size: usize, max: u64) -> Vec<u64> {
+    Uniform::new(0u64, max)
+        .sample_iter(thread_rng())
+        .take(size)
+        .collect::<Vec<u64>>()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let degree = 1 << 15;
+    let prime = 1152921504606748673u64;
+    let mut a = random_values(degree, prime);
+    let mut a1 = random_values(degree, prime);
+
+    let now = std::time::SystemTime::now();
+    for _ in 0..100000 {
+        elwise_mult_mod(&mut a, &a1, prime, degree as u64, 1);
+    }
+    println!("Time: {:?}", now.elapsed().unwrap() / 100000);
 }
